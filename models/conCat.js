0f842f4adbedd0984b961dbe191964ee8eebd5be
Nevin Lewis <lewisnevin@utexas.edu>
2023-01-03 21:47:31 -0600

    added formatting corrections

--- models/conCat.js
+++ models/conCat.js
@@ -1,33 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+
+/** Added model for concession categoires */
 class conCat extends Model {}
 
-// model below is for the Concession Categories
-
 conCat.init(
-  {
-      // Defining ID
+    {// Defining ID
       id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
       },
       // Defining category_name
       category_name: {
-          type: DataTypes.STRING,
-          allowNull: false
-      }
-  },
-  {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
       sequelize,
       timestamps: false,
       freezeTableName: true,
       underscored: true,
       modelName: 'category',
-  }
+    },
 );
-
 module.exports = conCat;
