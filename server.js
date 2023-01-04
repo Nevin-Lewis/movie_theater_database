29923b31f638b522d7fb43cdcd5bc4ee1df68058
Nevin Lewis <lewisnevin@utexas.edu>
2023-01-03 18:21:49 -0600

    added dependencies, some formatting changes and empty files to start filling out front end

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({helpers});
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,8 +24,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -36,11 +35,11 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
